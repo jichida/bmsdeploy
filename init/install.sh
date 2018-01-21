79838b7504212ecd1b8b569b14d86aa50a577a6a
wangxiaoqing <xiaoqingwang@126.com>
2018-01-22 01:27:30 +0800

    install -y

--- init/install.sh
+++ init/install.sh
@@ -1,10 +1,10 @@
 apt-get update
-apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
 apt-get update
-apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install docker-ce
+apt-get install docker-ce -y
 curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
