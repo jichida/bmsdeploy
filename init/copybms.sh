0dd8860b4468e53bc4f9d9c5d6987fd64081f5a2
wangxiaoqing <xiaoqingwang@126.com>
2018-01-25 01:57:41 +0800

    mongo & kafka

--- init/copybms.sh
+++ init/copybms.sh
@@ -1,4 +1,5 @@
-cd /root
-tar -czvf ./bmsdeploy.tar.gz ./bmsdeploy
-scp ./bmsdeploy.tar.gz root@192.168.1.114:/root/
-scp ./bmsdeploy.tar.gz root@192.168.1.136:/root/
+scp /root/bmsdeploy/ecs-7063-0001/docker-compose.yml root@192.168.1.20:/root/
+scp /root/bmsdeploy/ecs-7063-0002/docker-compose.yml root@192.168.1.114:/root/
+scp /root/bmsdeploy/ecs-7063-0003/docker-compose.yml root@192.168.1.136:/root/
+scp /root/bmsdeploy/ecs-7063-0004/docker-compose.yml root@192.168.1.45:/root/
+scp /root/bmsdeploy/ecs-7063-0005/docker-compose.yml root@192.168.1.31:/root/
