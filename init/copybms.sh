0a5aca1598a34fd9170e102f39f55329201a378d
wangxiaoqing <xiaoqingwang@126.com>
2018-02-02 22:54:30 +0800

    init.sh

--- init/copybms.sh
+++ init/copybms.sh
@@ -2,4 +2,5 @@ scp /root/bmsdeploy/ecs-7063-0001/docker-compose.yml root@192.168.1.20:/root/
 scp /root/bmsdeploy/ecs-7063-0002/docker-compose.yml root@192.168.1.114:/root/
 scp /root/bmsdeploy/ecs-7063-0003/docker-compose.yml root@192.168.1.136:/root/
 scp /root/bmsdeploy/ecs-7063-0004/docker-compose.yml root@192.168.1.45:/root/
-scp /root/bmsdeploy/ecs-7063-0005/docker-compose.yml root@192.168.1.31:/root/
+cp /root/bmsdeploy/ecs-7063-0005/docker-compose.yml /root/docker-compose.yml
+cp /root/bmsdeploy/ecs-7063-0005/nginx.conf /root/nginx.conf
