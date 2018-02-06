3538b178080e79c316182fb794fd95e2a7f56fc9
wangxiaoqing <xiaoqingwang@126.com>
2018-02-06 23:49:48 +0800

    +shell脚本

---
+++ ty/ecs-7063-0004/shellstartkafka.sh
@@ -0,0 +1,5 @@
+docker-composer -f ./kafkadb11.yml up -d
+docker-composer -f ./kafkadb21.yml up -d
+docker-composer -f ./kafkadb31.yml up -d
+docker-composer -f ./kafkadb41.yml up -d
+docker-composer -f ./kafkadb51.yml up -d
---
+++ ty/ecs-7063-0004/shellstopkafkadb.sh
@@ -0,0 +1,5 @@
+docker-composer -f ./kafkadb11.yml down
+docker-composer -f ./kafkadb21.yml down
+docker-composer -f ./kafkadb31.yml down
+docker-composer -f ./kafkadb41.yml down
+docker-composer -f ./kafkadb51.yml down
