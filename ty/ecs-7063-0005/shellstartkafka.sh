5caf223b2baac78514863bda9862352cc24f482b
wangxiaoqing <xiaoqingwang@126.com>
2018-02-06 23:54:22 +0800

    sh

--- ty/ecs-7063-0005/shellstartkafka.sh
+++ ty/ecs-7063-0005/shellstartkafka.sh
@@ -1,6 +1,6 @@
-docker-composer -f ./kafkadb0.yml up -d
-docker-composer -f ./kafkadb11.yml up -d
-docker-composer -f ./kafkadb21.yml up -d
-docker-composer -f ./kafkadb31.yml up -d
-docker-composer -f ./kafkadb41.yml up -d
-docker-composer -f ./kafkadb51.yml up -d
+docker-compose  -f ./kafkadb0.yml up -d
+docker-compose -f ./kafkadb11.yml up -d
+docker-compose -f ./kafkadb21.yml up -d
+docker-compose -f ./kafkadb31.yml up -d
+docker-compose -f ./kafkadb41.yml up -d
+docker-compose -f ./kafkadb51.yml up -d
