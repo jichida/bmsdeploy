63156846d2be65cdf635ffe9f5c2fc4ba0e6d94b
wangxiaoqing <xiaoqingwang@126.com>
2018-02-06 23:58:23 +0800

    remove 51

--- ty/ecs-7063-0005/shellstartkafka.sh
+++ ty/ecs-7063-0005/shellstartkafka.sh
@@ -3,4 +3,3 @@ docker-compose -f ./kafkadb11.yml up -d
 docker-compose -f ./kafkadb21.yml up -d
 docker-compose -f ./kafkadb31.yml up -d
 docker-compose -f ./kafkadb41.yml up -d
-docker-compose -f ./kafkadb51.yml up -d
--- ty/ecs-7063-0005/shellstopkafkadb.sh
+++ ty/ecs-7063-0005/shellstopkafkadb.sh
@@ -2,4 +2,3 @@ docker-compose -f ./kafkadb11.yml down
 docker-compose -f ./kafkadb21.yml down
 docker-compose -f ./kafkadb31.yml down
 docker-compose -f ./kafkadb41.yml down
-docker-compose -f ./kafkadb51.yml down
