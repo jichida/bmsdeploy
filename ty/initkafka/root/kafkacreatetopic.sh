bb43617da9f73961747c33ecae55c9122d85c90f
wangxiaoqing <xiaoqingwang@126.com>
2018-02-07 18:59:15 +0800

    resetconsumergroup

--- ty/initkafka/root/kafkacreatetopic.sh
+++ ty/initkafka/root/kafkacreatetopic.sh
@@ -4,6 +4,6 @@ kafka-topics --create --zookeeper  192.168.1.20:2181 --replication-factor 1 --pa
 kafka-topics --create --zookeeper  192.168.1.20:2181 --replication-factor 1 --partitions 48 --topic db.realtimealarms
 kafka-topics --create --zookeeper  192.168.1.20:2181 --replication-factor 1 --partitions 48 --topic db.realtimealarmraws
 kafka-topics --create --zookeeper  192.168.1.20:2181 --replication-factor 1 --partitions 48 --topic bms.index
-kafka-consumer-groups --zookeeper  192.168.1.20:2181 --group bmsmaster --reset-offsets --to-earliest --all-topics --execute
-kafka-consumer-groups --zookeeper  192.168.1.20:2181 --group bmsdb --reset-offsets --to-earliest --all-topics --execute
+kafka-consumer-groups --bootstrap-server 192.168.1.20:9092  --group bmsmaster --describe
+kafka-consumer-groups --bootstrap-server 192.168.1.20:9092  --group bmsdb --describe
 exit
