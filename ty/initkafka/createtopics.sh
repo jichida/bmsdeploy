4249bcf41133dcc997f2d78fba1e685ebc6fc517
wangxiaoqing <xiaoqingwang@126.com>
2018-02-07 18:48:24 +0800

    sh

--- ty/initkafka/createtopics.sh
+++ ty/initkafka/createtopics.sh
@@ -1,1 +1,1 @@
- docker run -it -v /root/bmsdeploy/ty/ecs-7063-0005/root:/root confluentinc/cp-kafka:4.0.0 bash -c "/root/kafkacreatetopic.sh && bash"
+ docker run -it -v /root/bmsdeploy/ty/initkafka/root:/root confluentinc/cp-kafka:4.0.0 bash -c "/root/kafkacreatetopic.sh && bash"
