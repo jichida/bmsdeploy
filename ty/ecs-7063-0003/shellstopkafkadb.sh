54b42ae15081309614887024a3bdc8b9db37aa74
wangxiaoqing <xiaoqingwang@126.com>
2018-02-09 23:52:01 +0800

    kafkadbdeploy

---
+++ ty/ecs-7063-0003/shellstopkafkadb.sh
@@ -0,0 +1,4 @@
+docker-compose -f ./kafkadb11.yml down
+docker-compose -f ./kafkadb21.yml down
+docker-compose -f ./kafkadb31.yml down
+docker-compose -f ./kafkadb41.yml down
--- ty/initkafka/root/kafkacreatetopic.sh
+++ ty/initkafka/root/kafkacreatetopic.sh
@@ -4,6 +4,5 @@ kafka-topics --create --zookeeper  192.168.1.20:2181 --replication-factor 1 --pa
 kafka-topics --create --zookeeper  192.168.1.20:2181 --replication-factor 1 --partitions 48 --topic db.realtimealarms
 kafka-topics --create --zookeeper  192.168.1.20:2181 --replication-factor 1 --partitions 48 --topic db.realtimealarmraws
 kafka-topics --create --zookeeper  192.168.1.20:2181 --replication-factor 1 --partitions 48 --topic bms.index
-kafka-consumer-groups --bootstrap-server 192.168.1.20:9092  --group bmsmaster --describe
 kafka-consumer-groups --bootstrap-server 192.168.1.20:9092  --group bmsdb --describe
 exit
