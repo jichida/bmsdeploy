dd705521c0662010433ca1c5d62ac3857f79f9fa
wangxiaoqing <xiaoqingwang@126.com>
2018-02-10 01:57:48 +0800

    reset

---
+++ ty/initkafka/printtopicoffset.sh
@@ -0,0 +1,1 @@
+docker run -it -v /root/bmsdeploy/ty/initkafka/root:/root confluentinc/cp-kafka:4.0.0 bash -c "/root/printtopicoffset.sh && bash"
---
+++ ty/initkafka/root/printtopicoffset.sh
@@ -0,0 +1,1 @@
+kafka-console-consumer --bootstrap-server 192.168.1.20:9092 --topic db.historydevices --offset 55 --partition 344 --max-messages 20
