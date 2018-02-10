a7fd179c15127d4067a0fa5a6515d9ecb4223c30
wangxiaoqing <xiaoqingwang@126.com>
2018-02-11 01:29:58 +0800

    init db shell

--- ty/init/initmongo/initdb.sh
+++ ty/init/initmongo/initdb.sh
@@ -1,1 +1,3 @@
-TMP_DIR=/tmp/mongodump ./mongo-restore ./bms.tar bmsnew
+rm -rf /tmp/mongodump && mkdir /tmp/mongodump
+tar -xzvf /root/bms.tar.gz -C  /tmp/mongodump
+docker run -it -v /tmp/mongodump:/tmp mongo:3.4 bash -c 'mongorestore --host 192.168.1.20:27007 --db bmsnew --dir /tmp/dump/bms'
