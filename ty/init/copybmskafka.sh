d6ea239cec4a0366fca0075a401205e599f11d05
wangxiaoqing <xiaoqingwang@126.com>
2018-02-16 15:15:31 +0800

    no message

--- ty/init/copybmskafka.sh
+++ ty/init/copybmskafka.sh
@@ -1,3 +1,3 @@
 scp /root/bmsdeploy/ty/ecs-7063-0001/kafka.yml root@192.168.1.20:/root/bmsdeploy/ty/ecs-7063-0001/kafka.yml
-scp /root/bmsdeploy/ty/ecs-7063-0001/kafka.yml root@192.168.1.114:/root/bmsdeploy/ty/ecs-7063-0001/kafka.yml
-scp /root/bmsdeploy/ty/ecs-7063-0001/kafka.yml root@192.168.1.136:/root/bmsdeploy/ty/ecs-7063-0001/kafka.yml
+scp /root/bmsdeploy/ty/ecs-7063-0002/kafka.yml root@192.168.1.114:/root/bmsdeploy/ty/ecs-7063-0002/kafka.yml
+scp /root/bmsdeploy/ty/ecs-7063-0003/kafka.yml root@192.168.1.136:/root/bmsdeploy/ty/ecs-7063-0003/kafka.yml
