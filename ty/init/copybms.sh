c7633652fadac438b598cc5d726c78f87e2a44c6
downup2u <25199344@qq.com>
2018-02-22 16:56:24 +0800

    ===

--- ty/init/copybms.sh
+++ ty/init/copybms.sh
@@ -2,7 +2,4 @@ cd /root
 rm fr ./*.tar.gz
 tar -czvf ./bms.tar.gz ./bms/deploy/kafkadb
 tar -czvf ./bmsdeploy.tar.gz ./bmsdeploy
-scp /root/*.tar.gz root@192.168.1.20:/root/
-scp /root/*.tar.gz root@192.168.1.114:/root/
-scp /root/*.tar.gz root@192.168.1.136:/root/
 scp /root/*.tar.gz root@192.168.1.45:/root/
