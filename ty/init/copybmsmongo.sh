71c735600425629950354acb565b62322dd3c60e
wangxiaoqing <xiaoqingwang@126.com>
2018-03-11 18:59:23 +0800

    cp mongo

---
+++ ty/init/copybmsmongo.sh
@@ -0,0 +1,3 @@
+scp /root/bmsdeploy/ty/ecs-7063-0001/mongodb.yml root@192.168.1.20:/root/bmsdeploy/ty/ecs-7063-0001/mongodb.yml
+scp /root/bmsdeploy/ty/ecs-7063-0002/mongodb.yml root@192.168.1.114:/root/bmsdeploy/ty/ecs-7063-0002/mongodb.yml
+scp /root/bmsdeploy/ty/ecs-7063-0003/mongodb.yml root@192.168.1.136:/root/bmsdeploy/ty/ecs-7063-0003/mongodb.yml
