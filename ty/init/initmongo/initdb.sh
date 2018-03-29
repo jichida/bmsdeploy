b25a7dab2f07b7aeb383b97b65ebc76be56eafab
wangxiaoqing <xiaoqingwang@126.com>
2018-03-29 10:21:12 +0800

    no message

--- ty/init/initmongo/initdb.sh
+++ ty/init/initmongo/initdb.sh
@@ -1,3 +1,3 @@
 rm -rf /tmp/mongodump && mkdir /tmp/mongodump
 tar -xzvf /root/bmsdb/bms.tar.gz -C  /tmp/mongodump
-docker run -it -v /tmp/mongodump:/tmp mongo:3.4 bash -c 'mongorestore --host 192.168.1.20:27007 --db bmsp --dir /tmp/dump/bms'
+docker run -it -v /tmp/mongodump:/tmp mongo:3.4 bash -c 'mongorestore --host 192.168.1.20:27007 --db bmscatl --dir /tmp/dump/bms'
